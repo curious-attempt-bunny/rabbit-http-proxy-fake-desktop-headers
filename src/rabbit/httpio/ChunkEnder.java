66f869578ec1e09b5bcf68f7952b4dbd94c349bb
Robert Olofsson <robo@khelekore.org>
2009-04-19 17:33:07 +0200

    Convert to NioHandler usage.

--- src/rabbit/httpio/ChunkEnder.java
+++ src/rabbit/httpio/ChunkEnder.java
@@ -2,11 +2,10 @@ package rabbit.httpio;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import rabbit.io.BufferHandle;
 import rabbit.io.SimpleBufferHandle;
-import rabbit.util.Logger;
+import rabbit.nio.NioHandler;
 import rabbit.util.TrafficLogger;
 
 /** A class that sends the chunk ending (with an empty footer).
@@ -17,12 +16,11 @@ public class ChunkEnder {
     private static final byte[] CHUNK_ENDING = 
     new byte[] {'0', '\r', '\n', '\r', '\n'};
 
-    public void sendChunkEnding (SocketChannel channel, Selector selector, 
-				 Logger logger, TrafficLogger tl, 
-				 BlockSentListener bsl) 
+    public void sendChunkEnding (SocketChannel channel, NioHandler nioHandler,
+				 TrafficLogger tl, BlockSentListener bsl) 
 	throws IOException {
 	ByteBuffer bb = ByteBuffer.wrap (CHUNK_ENDING);
 	BufferHandle bh = new SimpleBufferHandle (bb);
-	new BlockSender (channel, selector, logger, tl, bh, false, bsl);
+	new BlockSender (channel, nioHandler, tl, bh, false, bsl);
     }
 }
