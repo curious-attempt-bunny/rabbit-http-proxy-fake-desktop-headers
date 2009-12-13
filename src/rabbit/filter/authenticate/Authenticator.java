8857fb6afc8a6a15d55da9b707f020aea4f1a523
Robert Olofsson <robo@khelekore.org>
2009-12-13 16:24:46 +0100

    Remove param that is no longer used.

--- src/rabbit/filter/authenticate/Authenticator.java
+++ src/rabbit/filter/authenticate/Authenticator.java
@@ -13,7 +13,6 @@ public interface Authenticator {
     /** Try to authenticate the user.
      * @param user the username
      * @param pwd the password of the user
-     * @param channel the socket channel the user is currently using
      * @return true if authentication succeeded, false otherwise.
      */
     boolean authenticate (String user, String pwd);
