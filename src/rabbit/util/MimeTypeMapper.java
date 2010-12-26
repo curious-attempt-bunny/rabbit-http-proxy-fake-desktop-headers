8d8a7def3b39ca583ed64edbae3cc568afaade93
Robert Olofsson <robo@khelekore.org>
2010-12-26 16:21:41 +0100

    Add javadoc.

--- src/rabbit/util/MimeTypeMapper.java
+++ src/rabbit/util/MimeTypeMapper.java
@@ -5,6 +5,10 @@ package rabbit.util;
  * @author <a href="mailto:robo@khelekore.org">Robert Olofsson</a>
  */
 public class MimeTypeMapper {
+    /** Try to guess the mime type based on the given filename.
+     * @param filename the name of the file to guess the mime type for
+     * @return a mime type
+     */
     public static String getMimeType (String filename) {
 	filename = filename.toLowerCase ();
 	if (filename.endsWith ("gif"))
