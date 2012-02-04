ccb68285a72d32cd4f69113ce80fabb2a9ac3397
Robert Olofsson <robo@khelekore.org>
2012-02-04 22:21:12 +0100

    Make it possible to iterate over all the header values.

--- src/rabbit/http/GeneralHeader.java
+++ src/rabbit/http/GeneralHeader.java
@@ -5,6 +5,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import rabbit.io.Storable;
 
@@ -12,7 +13,7 @@ import rabbit.io.Storable;
  *
  * @author <a href="mailto:robo@khelekore.org">Robert Olofsson</a>
  */
-public class GeneralHeader implements Storable {    
+public class GeneralHeader implements Storable, Iterable<Header> {
 
     /** The headers of this Header in order.
      */
@@ -24,6 +25,10 @@ public class GeneralHeader implements Storable {
 	// empty
     }
 
+    public Iterator<Header> iterator () {
+	return headers.iterator ();
+    }
+
     /** Get the number of headers set in this header.
      * @return the number of header lines
      */
@@ -31,7 +36,7 @@ public class GeneralHeader implements Storable {
 	return headers.size ();
     }
 
-    /** Get the text value of this header 
+    /** Get the text value of this header
      * @return a String describing this GeneralHeader.
      */
     @Override public String toString () {
@@ -42,7 +47,7 @@ public class GeneralHeader implements Storable {
 
     /** Fill the given StringBuilder with text from this header.
      * @param sb the StringBuilder this header is written to
-     */ 
+     */
     protected void fillBuffer (StringBuilder sb) {
 	int hsize = headers.size ();
 	for (int i = 0; i < hsize; i++) {
@@ -55,7 +60,7 @@ public class GeneralHeader implements Storable {
 	sb.append (Header.CRLF);
     }
 
-    /** get the value of header type 
+    /** get the value of header type
      * @param type the Header were intrested in.
      * @return the value of type or null if no value is set.
      */
@@ -79,13 +84,13 @@ public class GeneralHeader implements Storable {
 	    if (h.getType ().equalsIgnoreCase (type)) {
 		h.setValue (value);
 		return;
-	    }	    
+	    }
 	}
 	Header h = new Header (type, value);
 	headers.add (h);
     }
 
-    /** Set a specified header 
+    /** Set a specified header
      * @param current the type or category that we want to set.
      * @param newValue the value we want to set
      */
@@ -96,26 +101,26 @@ public class GeneralHeader implements Storable {
 	    if (h.getValue ().equals (current)) {
 		h.setValue (newValue);
 		return;
-	    }	    
+	    }
 	}
     }
 
-    /** Add a new header. Old headers of the same type remain. 
+    /** Add a new header. Old headers of the same type remain.
      *  The new header is placed last.
      * @param type the type or category that we want to set.
      * @param value the value we want to set
      */
     public void addHeader (String type, String value) {
 	Header h = new Header (type, value);
-	addHeader (h);	
+	addHeader (h);
     }
 
-    /** Add a new header. Old headers of the same type remain. 
+    /** Add a new header. Old headers of the same type remain.
      *  The new header is placed last.
      * @param h the Header to add
      */
     public void addHeader (Header h) {
-	headers.add (h);	
+	headers.add (h);
     }
 
     /** removes a headerline from this header
@@ -133,7 +138,7 @@ public class GeneralHeader implements Storable {
 	}
     }
 
-    /** removes a header with the specified value 
+    /** removes a header with the specified value
      * @param value the value of the header we want to remove
      */
     public void removeValue (String value) {
@@ -152,7 +157,7 @@ public class GeneralHeader implements Storable {
      * @return all the headers lines of this header
      */
     public List<String> getHeaders (String type) {
-	List<String> ret = null; 
+	List<String> ret = null;
 	int s = headers.size ();
 	for (int i = 0; i < s; i++) {
 	    Header h = headers.get (i);
@@ -167,15 +172,15 @@ public class GeneralHeader implements Storable {
 	return ret;
     }
 
-    /** Copy all headers in this header to the given header. 
+    /** Copy all headers in this header to the given header.
      * @param to the GeneralHeader to add headers to.
      */
     public void copyHeader (GeneralHeader to) {
-        for (Header h : headers) {
-            to.addHeader (h.getType (), h.getValue ());
-        }
+	for (Header h : headers) {
+	    to.addHeader (h.getType (), h.getValue ());
+	}
     }
-    
+
     public void read (DataInput in) throws IOException {
 	int s = in.readInt ();
 	headers.ensureCapacity (s);
@@ -186,10 +191,9 @@ public class GeneralHeader implements Storable {
 	}
     }
 
-    public void write (DataOutput out) throws IOException { 
+    public void write (DataOutput out) throws IOException {
 	out.writeInt (headers.size ());
 	for (Header h : headers)
 	    h.write (out);
     }
 }
-    
